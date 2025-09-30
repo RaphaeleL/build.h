83e71b69b3028457cb3f65c891c7a55f92efd533
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-09-30 09:02:10 +0200

    000: indent

--- libaries/000_shl_logger.h
+++ libaries/000_shl_logger.h
@@ -15,7 +15,7 @@
 #define SHL_LOGGER_H
 
 #ifdef __cplusplus
-extern "C" {
+  extern "C" {
 #endif // __cplusplus
 
 // TODO: log to system log? (like journalctl)
@@ -35,70 +35,78 @@ typedef enum {
 // Initialize logger with minimum level
 void shl_init_logger(shl_log_level_t level);
 
-// Public logging macros
-#define shl_debug(fmt, ...) shl_log(SHL_LOG_DEBUG, fmt, ##__VA_ARGS__)
-#define shl_info(fmt, ...) shl_log(SHL_LOG_INFO, fmt, ##__VA_ARGS__)
-#define shl_warn(fmt, ...) shl_log(SHL_LOG_WARN, fmt, ##__VA_ARGS__)
-#define shl_error(fmt, ...) shl_log(SHL_LOG_ERROR, fmt, ##__VA_ARGS__)
-#define shl_critical(fmt, ...) shl_log(SHL_LOG_CRITICAL, fmt, ##__VA_ARGS__)
+// Use SHL logger if available
+#ifndef SHL_USE_LOGGER
+  #define shl_debug(fmt, ...) printf(fmt "\n", ##__VA_ARGS__)
+  #define shl_info(fmt, ...) printf(fmt "\n", ##__VA_ARGS__)
+  #define shl_warn(fmt, ...) printf(fmt "\n", ##__VA_ARGS__)
+  #define shl_error(fmt, ...) printf(fmt "\n", ##__VA_ARGS__)
+  #define shl_critical(fmt, ...) printf(fmt "\n", ##__VA_ARGS__)
+#else
+  #define shl_debug(fmt, ...) shl_log(SHL_LOG_DEBUG, fmt, ##__VA_ARGS__)
+  #define shl_info(fmt, ...) shl_log(SHL_LOG_INFO, fmt, ##__VA_ARGS__)
+  #define shl_warn(fmt, ...) shl_log(SHL_LOG_WARN, fmt, ##__VA_ARGS__)
+  #define shl_error(fmt, ...) shl_log(SHL_LOG_ERROR, fmt, ##__VA_ARGS__)
+  #define shl_critical(fmt, ...) shl_log(SHL_LOG_CRITICAL, fmt, ##__VA_ARGS__)
+#endif // SHL_USE _LOGGER
 
 // Strip prefix macros
 #ifdef SHL_STRIP_PREFIX
-#define init_logger   shl_init_logger
-#define debug         shl_debug
-#define info          shl_info
-#define warn          shl_warn
-#define error         shl_error
-#define critical      shl_critical
-#define LOG_NONE      SHL_LOG_NONE
-#define LOG_DEBUG     SHL_LOG_DEBUG
-#define LOG_INFO      SHL_LOG_INFO
-#define LOG_WARN      SHL_LOG_WARN
-#define LOG_ERROR     SHL_LOG_ERROR
-#define LOG_CRITICAL  SHL_LOG_CRITICAL
+  #define init_logger   shl_init_logger
+  #define debug         shl_debug
+  #define info          shl_info
+  #define warn          shl_warn
+  #define error         shl_error
+  #define critical      shl_critical
+  #define LOG_NONE      SHL_LOG_NONE
+  #define LOG_DEBUG     SHL_LOG_DEBUG
+  #define LOG_INFO      SHL_LOG_INFO
+  #define LOG_WARN      SHL_LOG_WARN
+  #define LOG_ERROR     SHL_LOG_ERROR
+  #define LOG_CRITICAL SHL_LOG_CRITICAL
 #endif // SHL_STRIP_PREFIX
 
 #ifdef SHL_IMPLEMENTATION
-#include <stdio.h>
-#include <time.h>
+  #include <stdio.h>
+  #include <time.h>
 
-// ANSI colors
-#define SHL_COLOR_RESET     "\x1b[0m"
-#define SHL_COLOR_DEBUG     "\x1b[90m" // gray
-#define SHL_COLOR_INFO      "\x1b[32m" // green
-#define SHL_COLOR_WARN      "\x1b[33m" // yellow
-#define SHL_COLOR_ERROR     "\x1b[31m" // red
-#define SHL_COLOR_CRITICAL  "\x1b[35m" // purple
+  // ANSI colors
+  #define SHL_COLOR_RESET     "\x1b[0m"
+  #define SHL_COLOR_DEBUG     "\x1b[90m" // gray
+  #define SHL_COLOR_INFO      "\x1b[32m" // green
+  #define SHL_COLOR_WARN      "\x1b[33m" // yellow
+  #define SHL_COLOR_ERROR     "\x1b[31m" // red
+  #define SHL_COLOR_CRITICAL  "\x1b[35m" // purple
 
-static shl_log_level_t shl_logger_min_level = SHL_LOG_INFO;
+  static shl_log_level_t shl_logger_min_level = SHL_LOG_INFO;
 
-void shl_init_logger(shl_log_level_t level) {
+  void shl_init_logger(shl_log_level_t level) {
     shl_logger_min_level = level;
-}
+  }
 
-static const char *shl_level_to_str(shl_log_level_t level) {
+  static const char *shl_level_to_str(shl_log_level_t level) {
     switch (level) {
-        case SHL_LOG_DEBUG:    return "DEBUG";
-        case SHL_LOG_INFO:     return "INFO";
-        case SHL_LOG_WARN:     return "WARN";
-        case SHL_LOG_ERROR:    return "ERROR";
-        case SHL_LOG_CRITICAL: return "CRITICAL";
-        default:               return "UNK";
+	case SHL_LOG_DEBUG:    return "DEBUG";
+	case SHL_LOG_INFO:     return "INFO";
+	case SHL_LOG_WARN:     return "WARN";
+	case SHL_LOG_ERROR:    return "ERROR";
+	case SHL_LOG_CRITICAL: return "CRITICAL";
+	default:               return "UNK";
     }
-}
+  }
 
-static const char *shl_level_to_color(shl_log_level_t level) {
+  static const char *shl_level_to_color(shl_log_level_t level) {
     switch (level) {
-        case SHL_LOG_DEBUG:    return SHL_COLOR_DEBUG;
-        case SHL_LOG_INFO:     return SHL_COLOR_INFO;
-        case SHL_LOG_WARN:     return SHL_COLOR_WARN;
-        case SHL_LOG_ERROR:    return SHL_COLOR_ERROR;
-        case SHL_LOG_CRITICAL: return SHL_COLOR_CRITICAL;
-        default:               return SHL_COLOR_RESET;
+	case SHL_LOG_DEBUG:    return SHL_COLOR_DEBUG;
+	case SHL_LOG_INFO:     return SHL_COLOR_INFO;
+	case SHL_LOG_WARN:     return SHL_COLOR_WARN;
+	case SHL_LOG_ERROR:    return SHL_COLOR_ERROR;
+	case SHL_LOG_CRITICAL: return SHL_COLOR_CRITICAL;
+	default:               return SHL_COLOR_RESET;
     }
-}
+  }
 
-void shl_log(shl_log_level_t level, const char *fmt, ...) {
+  void shl_log(shl_log_level_t level, const char *fmt, ...) {
     if (level < shl_logger_min_level || level >= SHL_LOG_NONE) return;
 
     // Timestamp
@@ -124,12 +132,12 @@ void shl_log(shl_log_level_t level, const char *fmt, ...) {
     va_end(args);
 
     fprintf(stderr, "\n");
-}
+  }
 
 #endif // SHL_IMPLEMENTATION
 
 #ifdef __cplusplus
-}
+  }
 #endif // __cplusplus
 
 #endif // SHL_LOGGER_H
