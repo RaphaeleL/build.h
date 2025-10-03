6dcd293c0c9f94c0c99b8a4c8672cebee754177a
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-10-03 10:18:05 +0200

    async auto run

--- build.c
+++ build.c
@@ -3,12 +3,11 @@
 #define SHL_USE_LOGGER
 #include "./build.h"
 
+#define async_auto_run false 
+
 int main() {
     init_logger((LogConfig_t){.level = LOG_INFO, .color = false});
     auto_rebuild();
-
-    // SystemConfig sysp_1 = { .command = "rm", .command_flags = "-rf out" };
-    // SystemConfig sysp_2 = { .command = "mkdir", .command_flags = "out" };
 
     const char *examples[][2] = {
         { "example/000_shl_logger.c",          "out/000_logger" },
@@ -23,21 +22,18 @@ int main() {
     size_t n_examples = sizeof(examples) / sizeof(examples[0]);
     BuildConfig builds[sizeof(examples) / sizeof(examples[0])];
 
-    // if (!run(&sysp_1)) return 1;
-    // if (!run(&sysp_2)) return 1;
-
     for (size_t i = 0; i < n_examples; i++) {
         builds[i] = (BuildConfig){
             .source   = examples[i][0],
             .output   = examples[i][1],
             .compiler = "gcc",
-            .autorun  = false,
-            .async    = false
+            .autorun  = async_auto_run,
+            .async    = async_auto_run 
         };
         if (!dispatch_build(&builds[i])) return 1;
     }
 
-    wait_for_all_builds();
+    if (async_auto_run) wait_for_all_builds();
 
     return 0;
 }
