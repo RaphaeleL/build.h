3ea6324ee73666dcac1bfc9251d32fda1fdf6005
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-12-28 15:53:54 +0100

    fix(build.c): prevent leaks

--- build.c
+++ build.c
@@ -15,21 +15,33 @@ int main() {
     const char* out_folder = "out";
 
     String contents = {0};
-    if (!read_dir(src_folder, &contents)) return EXIT_FAILURE;
+    if (!get_files_in_dir(src_folder, &contents)) return EXIT_FAILURE;
 
     for (size_t i = 0; i < contents.len; i++) {
         const char* src_path = contents.data[i];
-        
+
         if (!str_ends_with(src_path, ".c")) continue;
         const char* temp = str_replace(src_path, ".c", "");
         const char* new_path = str_replace(temp, src_folder, out_folder);
 
-        if (!temp || !new_path) continue;
+        if (!temp || !new_path) {
+            if (temp) free((void*)temp);
+            if (new_path) free((void*)new_path);
+            continue;
+        }
         cmd = default_c_build(src_path, new_path);
         if (is_windows && str_contains(src_path, "013_qol_thread_safety")) push(&cmd, "-pthread");
-        if (!run(&cmd, .procs = &procs)) return EXIT_SUCCESS;
+        if (!run(&cmd, .procs = &procs)) {
+            free((void*)temp);
+            free((void*)new_path);
+            release_string(&contents);
+            return EXIT_FAILURE;
+        }
+        free((void*)temp);
+        free((void*)new_path);
     }
 
+    release_string(&contents);
     if (!procs_wait(&procs)) return EXIT_FAILURE;
 
     // Build unittests
