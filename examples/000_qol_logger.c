2301473a00f479d9c319acc56724174bc42f2ac5
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2026-01-13 14:00:15 +0100

    chore(logger): update the example to include all possible log ways

--- examples/000_qol_logger.c
+++ examples/000_qol_logger.c
@@ -17,23 +17,33 @@
 #define QOL_STRIP_PREFIX
 #include "../build.h"
 
-int main() {
-    init_logger(.level=LOG_DIAG, .time=true, .color=true, .time_color=true); 
+void example(bool with_dead) {
     diag("This is a diag message\n");
     info("Starting program with pid=%d\n", 1234);
     exec("cc -o foo foo.c\n");
     hint("Starting program with pid=%d\n", 1234);
     warn("Low memory detected\n");
     erro("Fatal error: %s\n", "out of memory");
+    if (with_dead) dead("Fatal error: %s\n", "out of memory"); // this will exit the program
+}
+
+int main() {
+
+    init_logger(.level=LOG_DIAG, .time=true, .color=true, .time_color=true); 
+    printf("--------------------------\n");
+    example(false);
 
     init_logger(.level=LOG_DIAG, .time=true, .color=true); 
-    diag("This is a diag message\n");
-    info("Starting program with pid=%d\n", 1234);
-    exec("cc -o foo foo.c\n");
-    hint("Starting program with pid=%d\n", 1234);
-    warn("Low memory detected\n");
-    erro("Fatal error: %s\n", "out of memory");
-    dead("Fatal error: %s\n", "out of memory"); // this will exit the program
+    printf("--------------------------\n");
+    example(false);
+
+    init_logger(.level=LOG_DIAG, .time=false, .color=true); 
+    printf("--------------------------\n");
+    example(false);
+
+    init_logger(.level=LOG_DIAG, .time=false, .color=false); 
+    printf("--------------------------\n");
+    example(true);
 
     return EXIT_SUCCESS;
 }
